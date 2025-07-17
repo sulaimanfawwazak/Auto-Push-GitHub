a596d5db8f1aa51fafcfff0840c5d880d2456bae
sulaimanfawwazak <sulaimanfawwazak@gmail.com>
2025-07-18 06:10:15 +0700

    Fix: add gemini date validation for annoying wrong date format, perhaps

--- src/pages/api/fetch-history.js
+++ src/pages/api/fetch-history.js
@@ -1,14 +1,43 @@
 import { supabase } from "@/lib/supabase";
+import { GoogleGenAI } from '@google/genai';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env' });
 
 export default async function handler(req, res) {
   const tableName = 'history_events';
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  });
 
   const { date } = req.query;
-  if (!date || !/^\d{4}-\d{2}-\d{2}$/.test(date)) {
-    return res.status(400).json({ error: "Missing or invalid date format" });
+
+  console.log("date:", date);
+
+  // Added this to ensure the date format is correct using GenAI
+  const prompt = `Correct this date: ${date} to YYYY-MM-DD format if it's invalid. If it's already valid, return it unchanged. Respond ONLY with the corrected date string.`
+  
+  let correctDate;
+  try {
+    const date_response = await ai.models.generateContent({
+      model: 'gemini-1.5-flash',
+      contents: prompt
+    })
+
+    correctDate = date_response.text;
+    correctDate = correctDate.trim();
+
+    console.log("correctDate:", correctDate);
+
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(correctDate)) {
+      return res.status(400).json({ error: "Missing or invalid date format" });
+    }
+  }
+  catch (error) {
+    return res.status(500).json({ error: "Gemini error: " + error.message });
   }
 
-  const targetMD = date.slice(5); // YYYY-MM-DD -> MM-DD
+  const targetMD = correctDate.slice(5); // YYYY-MM-DD -> MM-DD
   
   try {
     const { data, error } = await supabase
